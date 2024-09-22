d554f914e8085faca0e19d3901bc5f7994e0b8a3
duhansysl <duhan2002@gmail.com>
2024-09-22 17:43:25 +0300

    apollo: uprev to V1.6

--- apollo.sh
+++ apollo.sh
@@ -40,7 +40,7 @@ CR_DTB=$CR_DIR/arch/$CR_ARCH/boot/dtb.img
 # defconfig dir
 CR_DEFCONFIG=$CR_DIR/arch/$CR_ARCH/configs
 # Kernel Name and Version
-CR_VERSION=V1.5.1
+CR_VERSION=V1.6
 CR_NAME=DS-ACK
 # Thread count
 CR_JOBS=$(nproc --all)
